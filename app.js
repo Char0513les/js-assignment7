3503a384d22079f7f6e7dcbba0ecf07ec90201b6
Char0513les <hicharlie0513@gmail.com>
2024-05-09 19:47:21 -0400

    first commit

--- app.js
+++ app.js
@@ -20,11 +20,28 @@ app.get('/', (_, response) => {
 
 
 // GET /api/todos
-
+app.get('/api/todos', (_, response) => {
+    response.json(todos);
+});
 // POST /api/todos
-
+app.post('/api/todos', (request, response) => {
+    const { item } = request.body;
+    const id = todos.length + 1;
+    const complete = false;
+    todos.push({ id, item, complete });
+    response.json({ id });
+});
 // PUT /api/todos/:id
-
+app.put('/api/todos/:id', (request, response) => {
+    const { id } = request.params;
+    const task = todos.find(todo => todo.id.toString() === id);
+    if (!task) {
+        response.status(404).json({ error: 'Task not found' });
+    } else {
+        task.complete = !task.complete; // Toggle completion status
+        response.json(task);
+    }
+});
 
 
 const message = `Server running: http://localhost:${port}`
